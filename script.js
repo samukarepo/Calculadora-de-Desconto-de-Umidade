23b9af7c020891032d6dc1a98ff4ad2ec34cd850
samukarepo <131726872+samukarepo@users.noreply.github.com>
2025-03-04 23:05:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcularDesconto(){
+    const peso = document.querySelector('#peso').value
+    const percentualDesconto = document.querySelector('#percentual').value
+    const percentualPeso = 100 - percentualDesconto
+    const pesoTotal = (peso * 100) / percentualPeso
+    const desconto = pesoTotal - peso
+
+    document.querySelector('#resultado').innerHTML = `O peso bruto é de <strong>${pesoTotal.toFixed(2)}</strong> Kg, e o deconto é <strong>${desconto.toFixed(2)}</strong> Kg!`    
+}
